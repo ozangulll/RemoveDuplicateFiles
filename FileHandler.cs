ccf4ab7fe4b77e6cd2b614786b573ce1bae6ee32
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-07-21 14:23:43 +0300

    Add project files.

---
+++ FileHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicate
+{
+    internal class FileHandler
+    {
+        private string _directoryPath;
+        private Dictionary<string, string> _fileNames;
+        private Regex _duplicateFilePattern;
+
+        public FileHandler(string directoryPath)
+        {
+            _directoryPath = directoryPath;
+            _fileNames = new Dictionary<string, string>();
+            _duplicateFilePattern = new Regex(@"\s\(\d+\)$");
+        }
+
+        public void RemoveDuplicateFilesByName()
+        {
+            var files = Directory.GetFiles(_directoryPath);
+            bool hasDuplicates = false;
+
+            foreach (var file in files)
+            {
+                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+                string fileExtension = Path.GetExtension(file);
+                string baseFileName = _duplicateFilePattern.Replace(fileNameWithoutExtension, "");
+                string fullBaseFileName = baseFileName + fileExtension;
+
+                if (_fileNames.ContainsKey(fullBaseFileName))
+                {
+                    Console.WriteLine($"Duplicate file found: {file}");
+                    File.Delete(file);
+                    hasDuplicates = true;
+                }
+                else
+                {
+                    _fileNames[fullBaseFileName] = file;
+                }
+            }
+
+            if (hasDuplicates)
+            {
+                Console.WriteLine("Duplicate files removed.");
+            }
+            else
+            {
+                Console.WriteLine("There are no duplicate files.");
+            }
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using RemoveDuplicate;
+
+class Program
+{
+    static void Main()
+    {
+        string directoryPath = @"C:\Users\Acer\Desktop\asa"; // Kendi dizin yolunuzu buraya yazın
+        FileHandler fileHandler = new FileHandler(directoryPath);
+        fileHandler.RemoveDuplicateFilesByName();
+    }
+
+    
+}
